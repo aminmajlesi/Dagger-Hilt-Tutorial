d19bae6fdd54713356b8cbf4165306c3be28d866
Amin Majlesi <majlesi@amnpardaz.com>
2023-01-05 11:10:03 +0330

    create application module

---
+++ app/src/main/java/com/example/daggerhilt/di/module/ApplicationModule.kt
@@ -0,0 +1,55 @@
+package com.example.daggerhilt.di.module
+
+import com.example.daggerhilt.BuildConfig
+import com.example.daggerhilt.data.api.ApiHelper
+import com.example.daggerhilt.data.api.ApiHelperImpl
+import com.example.daggerhilt.data.api.ApiService
+import com.example.daggerhilt.utils.Constants.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else OkHttpClient
+        .Builder()
+        .build()
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiHelper(apiHelper: ApiHelperImpl): ApiHelper = apiHelper
+
+}
---
+++ app/src/main/java/com/example/daggerhilt/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.daggerhilt.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://5e510330f2c0d300147c034c.mockapi.io"
+    }
+}
