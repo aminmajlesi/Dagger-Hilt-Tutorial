2e5df449ff00e240b58bacb6a53750b761f37261
Amin Majlesi <majlesi@amnpardaz.com>
2023-01-05 10:23:00 +0330

    make app class

--- app/src/main/java/com/example/daggerhilt/ui/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilt/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.daggerhilt
+package com.example.daggerhilt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.daggerhilt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/daggerhilt/utils/Status.kt
@@ -0,0 +1,8 @@
+package com.example.daggerhilt.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+
+}
